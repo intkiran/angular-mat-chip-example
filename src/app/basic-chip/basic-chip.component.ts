daae81c3236b3e49335e68657b5d1b3d4f52be90
intkiran <ikiranbabu@gmail.com>
2021-03-12 17:06:05 +0530

    3 files added, 2 deleted

---
+++ src/app/basic-chip/basic-chip.component.ts
@@ -0,0 +1,47 @@
+import { COMMA, ENTER } from "@angular/cdk/keycodes";
+import { Component, OnInit } from "@angular/core";
+import { MatChipInputEvent } from "@angular/material";
+
+export interface Country {
+  name: string;
+}
+@Component({
+  selector: "app-basic-chip",
+  templateUrl: "./basic-chip.component.html",
+  styleUrls: ["./basic-chip.component.css"]
+})
+export class BasicChipComponent {
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+  countries: Country[] = [
+    { name: "India" },
+    { name: "USA" },
+    { name: "Apple" }
+  ];
+
+  add(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+
+    // Add our fruit
+    if ((value || "").trim()) {
+      this.countries.push({ name: value.trim() });
+    }
+
+    // Reset the input value
+    if (input) {
+      input.value = "";
+    }
+  }
+
+  remove(country: Country): void {
+    const index = this.countries.indexOf(country);
+
+    if (index >= 0) {
+      this.countries.splice(index, 1);
+    }
+  }
+}
